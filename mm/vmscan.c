eb608af6bc91e645e18f86a7165e16b742de1ad3
officialputuid <officialputuid@hack.id>
2022-05-10 10:55:40 +0000

    vm: Reduce vm swappiness
    Change-Id: I0b6df58da9b4bbd6dba792a8ec3be7a8b20c1ba0

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -172,7 +172,7 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 40;
 
 /*
  * The total number of pages which are beyond the high watermark within all
@@ -182,7 +182,7 @@ int vm_swappiness = 60;
 /*
  * Direct reclaim swappiness, exptct 0 - 60. Higher means more swappy and slower.
  */
-int direct_vm_swappiness = 60;
+int direct_vm_swappiness = 40;
 #endif
 unsigned long vm_total_pages;
 
